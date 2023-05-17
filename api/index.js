3a086dc4247fc091b6784229a8aed1efedccc0bf
boboPrem1 <boboprem1@gmail.com>
2023-05-17 03:33:51 +0000

    Add the jock express app files

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express, { json } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    1: "Juvanio Kangni Juvanio",
+    2: "Is gonna be",
+    3: "The best developper in the world",
+    4: "horizon 2030",
+    5: "I'm gonna be a billionnaire, I'm sure of it",
+    6: "make sense of it all ?",
+  });
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
